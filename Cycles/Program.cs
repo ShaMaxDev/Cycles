b4e1e77e5f1433970a8199f1d4b0f41cfa868990
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-15 21:39:51 -0700

    Add project files.

---
+++ Cycles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What do you want me to repeat?");
+            string userMessage = Console.ReadLine();
+            Console.WriteLine("Ok. How many times?");
+            int repeatNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ok. Here we go:");
+
+            while (repeatNumber-- > 0 ) 
+            {
+                Console.WriteLine(userMessage);
+            }
+            Console.WriteLine("That's it.");
+            Console.ReadKey();
+        }
+    }
+}
